4242e4de59d154e6393907f3d3e94f19aff7b2eb
markovchainy <markovchainy@users.noreply.github.com>
2018-02-23 07:59:15 +0000

    Added search paths for clang-tidy

--- clang-tidy.sh
+++ clang-tidy.sh
@@ -6,7 +6,15 @@ then
     exit 1
 fi
 
-CLANG_TIDY=`locate run-clang-tidy.py`
+CLANG_TIDY=`locate run-clang-tidy.py 2>/dev/null`
+
+for SEARCH_PATH in /usr/bin/;
+do
+    if [ "$CLANG_TIDY" == "" ]; then
+        CLANG_TIDY=`ls $SEARCH_PATH/run-clang-tidy*.py | head -1`
+    fi
+done
+
 if [ "$CLANG_TIDY" == "" ]; then
     echo "ERROR: run-clang-tidy.py not found on system"
     exit -1
