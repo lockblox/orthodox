882bdfe1891464be6c11775e73785f17f3877369
markovchainy <markovchainy@users.noreply.github.com>
2019-01-05 18:39:09 +0000

    Removed optimisations flag for coverage build

--- build.sh
+++ build.sh
@@ -58,7 +58,7 @@ echo Configuring build \
  && time make -j ${CPUS} \
  && echo Running tests with thread sanitizer \
  && time make test CTEST_OUTPUT_ON_FAILURE=TRUE \
- && cmake -DCMAKE_BUILD_TYPE=Debug -DCMAKE_CXX_FLAGS="--coverage -O0" \
+ && cmake -DCMAKE_BUILD_TYPE=Debug -DCMAKE_CXX_FLAGS="--coverage" \
     -DCMAKE_CXX_COMPILER=/usr/bin/clang++ ${ROOT_DIR} \
  && echo Building with coverage \
  && time make -j ${CPUS} \
