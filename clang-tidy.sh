8f1eb360889dea27e7692c99c76d8e6d4a153921
markovchainy <markovchainy@users.noreply.github.com>
2021-12-29 11:09:30 +0000

    Created clang-tidy.sh

--- clang-tidy.sh
+++ clang-tidy.sh
@@ -1,31 +1,9 @@
 #!/bin/bash
 
-if [ $# -ne 2 ];
-then
-    echo Usage: clang-tidy.sh SOURCE_DIR BUILD_DIR
-    exit 1
-fi
+TOOLSDIR=`dirname $0`
+source ${TOOLSDIR}/common.sh
 
-CLANG_TIDY=`locate run-clang-tidy.py 2>/dev/null`
+BUILDDIR=${BUILDROOT}/cmake-clang-tidy
 
-for SEARCH_PATH in /usr/bin/ /usr/share/clang;
-do
-    if [ "$CLANG_TIDY" == "" ]; then
-        CLANG_TIDY=`ls $SEARCH_PATH/run-clang-tidy*.py | head -1`
-    fi
-done
-
-if [ "$CLANG_TIDY" == "" ]; then
-    echo "ERROR: run-clang-tidy.py not found on system"
-    exit -1
-else
-    echo "Using ${CLANG_TIDY}"
-fi
-
-# Find sources
-SOURCE_DIR=`readlink -f $1`/
-BUILD_DIR=$2
-
-$CLANG_TIDY -p "$BUILD_DIR"
-
-exit $?
+export CMAKE_CONFIG_ARGS="-DCMAKE_CXX_CLANG_TIDY=clang-tidy ${CMAKE_CONFIG_ARGS}"
+run_cmake ${BUILDDIR}
