35254ea4db6e1e49d221472ecd3173685c27606d
markovchainy <markovchainy@users.noreply.github.com>
2021-12-29 11:10:28 +0000

    Created cpp-check.sh

---
+++ cpp-check.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+TOOLSDIR=`dirname $0`
+source ${TOOLSDIR}/common.sh
+
+BUILDDIR=${BUILDROOT}/cmake-build-cpp-check
+
+CPPCHECK_ARGS="cppcheck;\
+--cppcheck-build-dir=${BUILDDIR};\
+--suppressions-list=${SOURCEDIR}/.cpp-check-ignore;\
+--inline-suppr;\
+--error-exitcode=1;\
+--force;\
+--enable=all"
+export CMAKE_CONFIG_ARGS="-DCMAKE_CXX_CPPCHECK=\"${CPPCHECK_ARGS}\" ${CMAKE_CONFIG_ARGS}"
+
+run_cmake ${BUILDDIR}
