96305c55e550f3bd0d18d166bcf9d3c4b44f5128
markovchainy <markovchainy@users.noreply.github.com>
2022-06-26 21:49:12 +0100

    Created clang script

---
+++ clang.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+TOOLSDIR=`dirname $0`
+source ${TOOLSDIR}/common.sh
+
+echo Using CLANG flags: ${CLANG_FLAGS}
+
+BUILDDIR=${BUILDROOT}/cmake-clang
+CLANG_FLAGS="--system-header-prefix=boost 
+            -Weverything 
+            -Werror 
+            -Wno-c++98-compat 
+            -Wno-c++98-compat-pedantic 
+            -Wno-clang-diagnostic-unknown-warning-option"
+
+export CC=clang
+export CXX=clang++
+export CCC_CC=${CC}
+export CCC_CXX=${CXX}
+export CMAKE_CONFIG_ARGS="-DCMAKE_CXX_FLAGS=\"${CLANG_FLAGS}\" ${CMAKE_CONFIG_ARGS}"
+export CMAKE_CONFIG_ARGS="-DCMAKE_THREAD_LIBS_INIT=\"-lpthread\" ${CMAKE_CONFIG_ARGS}"
+
+run_cmake ${BUILDDIR} \
+&& run_ctest ${BUILDDIR}
