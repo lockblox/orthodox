cbe9fb8119b2b22127c3e51662dd2a0264ec0d8f
markovchainy <markovchainy@users.noreply.github.com>
2022-08-28 16:11:22 +0100

    Make iwyu fail on errors

--- include-what-you-use.sh
+++ include-what-you-use.sh
@@ -5,21 +5,21 @@ set -u
 TOOLSDIR=`dirname $0`
 source ${TOOLSDIR}/common.sh
 
-BUILDDIR=${BUILDROOT}/cmake-iwyu
+BUILDDIR=${BUILDROOT}/${PROJECT}-cmake-iwyu
 
 IWYU_ARGS="include-what-you-use"
 export CMAKE_CONFIG_ARGS="-DCMAKE_CXX_INCLUDE_WHAT_YOU_USE=\"${IWYU_ARGS}\" \
                          -DCMAKE_LINK_WHAT_YOU_USE_CHECK=TRUE
                          ${CMAKE_CONFIG_ARGS}"
+export CMAKE_RUN_ARGS="--clean-first"
 
 logfile=`mktemp`
 run_cmake ${BUILDDIR} | tee $logfile
 
-cat ${logfile} | while read line;
-do 
-    echo ${line} | grep -v "Warning: include-what-you-use" > /dev/null
-    if [ $? -ne 0 ];
-    then
-        exit 1
-    fi
-done
+error_count=`grep "Warning: include-what-you-use" ${logfile} | wc -l`
+rm ${logfile}
+
+if [ ${error_count} -ne 0 ];
+then
+    exit 1
+fi
