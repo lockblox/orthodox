bd43c5ccfd68ddd2816977247c908edfcb462d03
markovchainy <markovchainy@users.noreply.github.com>
2022-08-28 16:11:09 +0100

    Make cpp-lint fail on errors

--- cpp-lint.sh
+++ cpp-lint.sh
@@ -1,12 +1,24 @@
 #!/bin/bash
 
+set -u 
+
 TOOLSDIR=`dirname $0`
 source ${TOOLSDIR}/common.sh
 
-BUILDDIR=${BUILDROOT}/cmake-cpp-lint
+BUILDDIR=${BUILDROOT}/${PROJECT}-cmake-cpp-lint
 
-CPP_LINT_ARGS="cpplint"
+CPP_LINT_ARGS="cpplint;--quiet;--counting=detailed"
 export CMAKE_CONFIG_ARGS="-DCMAKE_CXX_CPPLINT=\"${CPP_LINT_ARGS}\" \
                          ${CMAKE_CONFIG_ARGS}"
+export CMAKE_RUN_ARGS="--clean-first"
 
-run_cmake ${BUILDDIR}
+logfile=`mktemp`
+run_cmake ${BUILDDIR} | tee $logfile
+
+error_count=`grep "errors found" ${logfile} | wc -l`
+rm ${logfile}
+
+if [ ${error_count} -ne 0 ];
+then
+    exit 1
+fi
