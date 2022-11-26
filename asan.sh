9ec29c566104f597991226c4fd0a4732fd0c117b
markovchainy <markovchainy@users.noreply.github.com>
2022-11-26 11:23:02 +0000

    Include analyzer and symbolizer into ASAN execution

--- asan.sh
+++ asan.sh
@@ -9,8 +9,12 @@ ASAN_FLAGS="${ASAN_FLAGS} -fno-omit-frame-pointer -fno-optimize-sibling-calls -W
 
 echo Using ASAN flags: ${ASAN_FLAGS}
 
+export ANALYZER=`locate c++-analyzer | egrep "c\+\+-analyzer$" | head -1`
+export LSAN_OPTIONS=verbosity=1:log_threads=1
+export ASAN_SYMBOLIZER_PATH=`locate llvm-symbolizer | egrep "llvm-symbolizer$" | head -1`
 export BUILDDIR=${BUILDROOT}/${PROJECT}-cmake-asan
 export CMAKE_CONFIG_ARGS="-DCMAKE_CXX_FLAGS=\"${ASAN_FLAGS}\" ${CMAKE_CONFIG_ARGS}"
+export CMAKE_CONFIG_ARGS="-DCMAKE_CXX_COMPILER=${ANALYZER} ${CMAKE_CONFIG_ARGS}"
 
 run_cmake ${BUILDDIR} \
 && run_ctest ${BUILDDIR}
