0967c54ed80295eb441b29b64e9cb939896371d3
Markov Chainy <markovchainy@protonmail.com>
2023-12-17 23:44:38 +0000

    Use cpp-check-ignore only if it exists

--- cpp-check.sh
+++ cpp-check.sh
@@ -5,9 +5,16 @@ source ${TOOLSDIR}/common.sh
 
 BUILDDIR=${BUILDROOT}/${PROJECT}-cmake-cpp-check
 
+CPPCHECK_IGNORE=""
+if [ -f ${SOURCEDIR}/.cpp-check-ignore ];
+then
+    CPPCHECK_IGNORE="--suppressions-list=${SOURCEDIR}/.cpp-check-ignore"
+fi
+
 CPPCHECK_ARGS="cppcheck;\
 --cppcheck-build-dir=${BUILDDIR};\
 --suppressions-list=${SOURCEDIR}/.cpp-check-ignore;\
+${CPPCHECK_IGNORE};\
 --inline-suppr;\
 --error-exitcode=1;\
 --force;\
